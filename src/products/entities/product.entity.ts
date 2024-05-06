b056d394f21aba3aa3fbfeb78423866903e4f55b
Brayan1951 <brayanc1951@gmail.com>
2024-05-06 07:52:06 -0500

    First commit

---
+++ src/products/entities/product.entity.ts
@@ -0,0 +1,12 @@
+export class Product {
+
+    public id:number;
+
+    public name:String;
+
+    public price:number;
+
+
+
+
+}
---
+++ src/products/products.service.ts
@@ -0,0 +1,78 @@
+import { Injectable, Logger, NotFoundException, OnModuleInit } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { PrismaClient } from '@prisma/client';
+import { PaginationDto } from '../common/dto/pagination.dto';
+
+@Injectable()
+export class ProductsService  extends PrismaClient implements OnModuleInit{
+
+  private readonly logger=new Logger('ProductsService')
+
+  onModuleInit() {
+ 
+    this.$connect();
+    this.logger.log("Database Connected");
+    
+  }
+  create(createProductDto: CreateProductDto) {
+    // return 'This action adds a new product';
+    return this.product.create({
+      data: createProductDto
+    });   
+  }
+
+  async findAll(paginationDto:PaginationDto) {
+    const {page,limit}=paginationDto
+
+    const totalPage=await this.product.count({where:{available:true}});
+    const lastPage=Math.ceil(totalPage/limit)
+
+    return{ 
+      data:await this.product.findMany({
+      skip:(page-1)*limit,
+      take:limit,
+      where:{available:true}
+    }),
+    meta:{
+      total:totalPage,
+      page,
+      lastPage
+    }
+  }
+
+  }
+
+  async findOne(id: number) {
+
+    const product=await this.product.findUnique({
+      where:{id,available:true}
+    });
+    if (!product) {
+      throw new NotFoundException(`Product with id ${id} not found`)
+    }
+    return   product;
+  }
+
+  async update(id: number, updateProductDto: UpdateProductDto) {
+    
+    const {id:__,...data}=updateProductDto
+    await this.findOne(id);
+    return this.product.update({where:{id},data:data})
+    
+  }
+  
+  async remove(id: number) {
+    await this.findOne(id);
+    
+    // return this.product.delete({where:{id}});
+    
+    const product=await this.product.update({
+      where:{id},
+      data:{available:false}
+    })
+
+    return product
+    
+  }
+}
